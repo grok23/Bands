f2676adae3d704ab9c12b6b5a965a05165bf9e71
grok23 <46896171+grok23@users.noreply.github.com>
2020-01-21 13:02:54 +0000

    Add project files.

---
+++ Bands/Band.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bands
+{
+    class Band: IComparable
+    {
+        public string BandName { get; set; }
+        public DateTime YearFormed { get; set; }
+        public string Members { get; set; }
+
+
+
+
+
+        //method to enable sorting by band names
+        public int CompareTo(object obj)
+        {
+            //get reference to next object in list
+            Band objectToCompareTo = obj as Band;
+
+            //compare to is set to compare the ActivityDate
+            int returnValue = this.BandName.CompareTo(objectToCompareTo.BandName);
+            return returnValue;
+        }
+    }
+}
---
+++ Bands/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bands
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //create lists to hold all bands available
+        List<Band> bands = new List<Band>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateBands();
+        }
+        private void MainWindow1_Loaded(object sender, RoutedEventArgs e) //create band objects list and sort it
+        {
+
+            CreateBands();
+            bands.Sort();                              //sorts the bands list using the icomparable compareTo
+            
+        }
+        private List<Band> CreateBands()           //method to create and return the list of bands
+        {
+            //band objects are created
+            Band B1 = new Band()
+            {
+                BandName = "Shpongle",
+                YearFormed = new DateTime(1996, 06, 01),
+                Members = "Simon Posford, Raja Ram",
+            };
+            Band B2 = new Band()
+            {
+                BandName = "Younger Brother",
+                YearFormed = new DateTime(2003, 06, 01),
+                Members = "Simon Posford, Benji Vaughan",
+            };
+
+            //add the above bands to the bands list
+            bands.Add(B1);
+            bands.Add(B2);
+            
+            //return the completed list
+            return bands;
+
+        }
+    }
+}
